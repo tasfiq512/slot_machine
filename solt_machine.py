0456d24f148fb0b6fa5e7291b149cfb33439c597
tasfiq512 <tasfiq512@gmail.com>
2024-02-03 01:26:20 +0600

    deposite

---
+++ solt_machine.py
@@ -0,0 +1,36 @@
+def deposit():
+    while True:
+        amount=input("What would you like to deposit? $ ")
+        if amount.isdigit():
+            amount=int(amount)
+            if amount>0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number .")
+    return amount
+def get_number_of_line():
+    while True:
+        lines=input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines=int(lines)
+            if 1<= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number .")
+    return lines
+def get_bet():
+    while True:
+        amount=input("What would you like to bet on each line ? $ ")
+        if amount.isdigit():
+            amount=int(amount)
+            if MIN_BET<= amount<=MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET} .")
+        else:
+            print("Please enter a number .")
+    return amount
