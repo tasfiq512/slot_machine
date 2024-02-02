f67931d4d8ed6a1c79a6e17635632b6d8e67863a
tasfiq512 <tasfiq512@gmail.com>
2024-02-03 01:28:09 +0600

    finished

--- solt_machine.py
+++ solt_machine.py
@@ -1,3 +1,58 @@
+import random
+MAX_LINES=3
+MAX_BET=100
+MIN_BET=1
+ROWS=3
+COLS=3
+symbol_value={
+   "A":5,
+   "B":4,
+   "C":3,
+   "D":2
+
+}
+def check_winning(columns,lines,bet,values):
+    winnings=0
+    winning_lines=[]
+    for line in range(lines):
+        symbol=columns[0][lines]
+        for column in columns:
+            symbol_to_check=column[lines]
+            if symbol!= symbol_to_check:
+                break
+            else:
+                winnings += values[symbol]* bet
+                winning_lines.append(line+1)
+    return winnings,winning_lines
+
+def get_slot_machine_spin(rows,cols,symbols):
+    all_symbols=[]
+    for symbol,symbol_count in symbols.items():
+        for _ in range (symbol_count):
+            all_symbols.append(symbol)
+    
+    columns=[]
+    for _ in range(cols):
+        column=[]
+        current_symbols=all_symbols[:]
+
+        for _ in range(rows):
+            value=random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+    return columns
+
+def print_slot_machine(columns):
+    for row in range (len(columns[0])):
+        for i, column in enumerate(columns):
+            if i !=len(columns)-1:
+               print(column[row], end=" | ")
+            else:
+                print(column[row],end="")
+        print()
+
+
 def deposit():
     while True:
         amount=input("What would you like to deposit? $ ")
